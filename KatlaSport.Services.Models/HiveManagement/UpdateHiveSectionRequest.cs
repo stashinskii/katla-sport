b00208e91c7ab1052e9b12eace2d829dc8a653f9
Herman <germanstashinskii@gmail.com>
2018-10-07 21:25:54 +0300

    UpdateHiveSectionRequest were done and registered

---
+++ KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace KatlaSport.Services.HiveManagement
+{
+    public class UpdateHiveSectionRequest
+    {
+        /// <summary>
+        /// Gets or sets a store hive name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a store hive code.
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
--- KatlaSport.Services/HiveManagement/HiveManagementMappingProfile.cs
+++ KatlaSport.Services/HiveManagement/HiveManagementMappingProfile.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using AutoMapper;
-
 using DataAccessHive = KatlaSport.DataAccess.ProductStoreHive.StoreHive;
 using DataAccessHiveSection = KatlaSport.DataAccess.ProductStoreHive.StoreHiveSection;
 
@@ -15,6 +14,8 @@ namespace KatlaSport.Services.HiveManagement
             CreateMap<DataAccessHiveSection, HiveSectionListItem>();
             CreateMap<DataAccessHiveSection, HiveSection>();
             CreateMap<UpdateHiveRequest, DataAccessHive>()
+                .ForMember(r => r.LastUpdated, opt => opt.MapFrom(p => DateTime.UtcNow));
+            CreateMap<UpdateHiveSectionRequest, DataAccessHiveSection>()
                 .ForMember(r => r.LastUpdated, opt => opt.MapFrom(p => DateTime.UtcNow));
         }
     }
